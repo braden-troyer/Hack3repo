c466a99486c6ef26d6065f0f4493b76dbdc65fbf
Braden <braden.troyer04@gmail.com>
2021-06-27 03:36:21 -0500

    Fix potential no-file error

--- graph.py
+++ graph.py
@@ -1,25 +1,31 @@
 import matplotlib.pyplot as plt
 import csv
+from os.path import exists
 
-x=[]
-y=[]
+filename = 'statistics.csv'
 
-with open('statistics.csv', 'r') as csvfile:
-    plots= csv.reader(csvfile, delimiter=',')
-    counter = 0
-    for row in plots:
-        if counter == 0:
-            counter += 1
-        else:
-            x.append(int(row[1]))
-            y.append(int(row[2]))
+if exists(filename):
+    x=[]
+    y=[]
 
-print(x)
-plt.plot(x,y, marker='o')
+    with open(filename, 'r') as csvfile:
+        plots= csv.reader(csvfile, delimiter=',')
+        counter = 0
+        for row in plots:
+            if counter == 0:
+                counter += 1
+            else:
+                x.append(int(row[1]))
+                y.append(int(row[2]))
 
-plt.title('Data from the CSV File: Typing')
+    print(x)
+    plt.plot(x,y, marker='o')
 
-plt.xlabel('Time(In Minutes)')
-plt.ylabel('Characters Typed')
+    plt.title('Data from the CSV File: Typing')
 
-plt.show()
+    plt.xlabel('Time(In Minutes)')
+    plt.ylabel('Characters Typed')
+
+    plt.show()
+else:
+    print("File Does Not Exist")
